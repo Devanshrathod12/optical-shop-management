2416e8159fe0c3c04828cdeb7dafadab6b2ffcb4
devansh rathod <rathoddevansh53@gmail.com>
2025-08-09 23:23:46 +0530

    api work perfactly no show any error

--- optical-shop-backend/routes/userRoutes.js
+++ optical-shop-backend/routes/userRoutes.js
@@ -2,8 +2,8 @@ const express = require("express");
 const { signup, login } = require("../controllers/userController");
 const router = express.Router();
 
-router.post("/signup", signup);
+router.post("/signup", signup); // working fine 09/08/2025
 
-router.post("/login", login);
+router.post("/login", login); // working fine 09/08/2025
 
 module.exports = router;
